e86ba7a5800b49c83d8352cd2d9d054099232b6d
prakash <p.sutar1995@gmail.com>
2021-10-24 12:37:49 +0530

    Intial commit

---
+++ src/main/java/Git_Project/Git_Project_For_GitHub/App.java
@@ -0,0 +1,13 @@
+package Git_Project.Git_Project_For_GitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Git_Project/Git_Project_For_GitHub/LoginTest.java
@@ -0,0 +1,48 @@
+package Git_Project.Git_Project_For_GitHub;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+//secure shell cryptography
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumDrivers\\chromedriver.exe");
+		driver driver=new ChromeDriver();
+	}
+	
+	
+	@Test
+	public void doLogin()
+	{
+		System.out.println("Executing Login test");
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(20L,  TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[id='email']")).sendKeys("p.sutar1995@gmail.com");
+		driver.findElement(By.xpath("//*[id='pass']")).sendKeys("abcd");
+		
+		driver.findElement(By.name("login")).click();
+		
+		System.out.println("Log into facebook account");
+		
+		
+		
+	}
+	
+	@AfterTest
+	public void QuitDriver()
+	{
+		if (driver!=null)
+		driver.close();
+	}
+}
